e6354a13716e637b392647bdb0ae21bd293ffb1e
Gayan <89770956+EA-Gayan@users.noreply.github.com>
2025-04-29 17:28:24 +0530

    added database connection

--- app/layout.js
+++ app/layout.js
@@ -2,14 +2,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "500"] });
 
